ece39424f8499a85f5662d8ee9a640524a1b9b79
GlagolSpirinS <112942625+GlagolSpirinS@users.noreply.github.com>
2023-11-24 00:58:12 +0300

    salat

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,13 @@ import main_app.views
 from main_app import views
 
 urlpatterns = [
-    path('', views.index, name="main")
+    path('', views.index, name="main"),
+    path('about/', views.about, name="about"),
+    path('blog-home/', views.blog_home, name='blog_home'),
+    path('blog-post/', views.blog_post, name='blog_post'),
+    path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
+    path('portfolio-item/', views.portfolio_item, name='portfolio_item'),
+    path('portfolio-overview/', views.portfolio_overview, name='portfolio_overview'),
+    path('pricing/', views.pricing, name='pricing'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def blog_home(request):
+    return render(request, "blog-home.html")
+
+def blog_post(request):
+    return render(request, "blog-post.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def faq(request):
+    return render(request, "faq.html")
+
+def portfolio_item(request):
+    return render(request, "portfolio-item.html")
+
+def portfolio_overview(request):
+    return render(request, "portfolio-overview.html")
+
+def pricing(request):
+    return render(request, "pricing.html")
